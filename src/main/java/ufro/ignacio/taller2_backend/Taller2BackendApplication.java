3bb7fee463fd8aa6377de61709060a887f277f9d
Ignacio-Us <i.uribe01@ufromail.cl>
2024-06-12 13:58:46 -0400

    Initial commit

---
+++ src/main/java/ufro/ignacio/taller2_backend/Taller2BackendApplication.java
@@ -0,0 +1,13 @@
+package ufro.ignacio.taller2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufro/ignacio/taller2_backend/Taller2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ufro.ignacio.taller2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
